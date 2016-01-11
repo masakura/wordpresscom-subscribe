f162a635cae3a3c5326b8e12bd8960b559aa8197
Tomo Masakura <tomo.masakura@gmail.com>
2016-01-11 23:12:50 +0900

    シンプルに、一サイトのみ、ソートなし表示

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,86 +24,23 @@
     return;
   }
 
-  /**
-   * サイトの投稿一覧を取得します
-   * @param site {String} サイト ID ('masakura.wordpress.com', or 1234)
-   * @return {Deferred} 結果を返す
-   */
-  function getPosts(site) {
-    return $.ajax({
-    url: 'https://public-api.wordpress.com/rest/v1/sites/' + site + '/posts/',
-    type: 'GET',
-    beforeSend: function (xhr) {
-      xhr.setRequestHeader('Authorization', 'BEARER ' + params['#access_token']);
-    }
+  $(document).on('submit', '#subscribe', function () {
+    var site = $('#site_id').val();
+
+    $.ajax({
+      url: 'https://public-api.wordpress.com/rest/v1/sites/' + site + '/posts/',
+      type: 'GET',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', 'BEARER ' + params['#access_token']);
+      }
     })
       .then(function (data) {
-        return data.posts;
+        var posts = data.posts;
+        var $posts = posts.map(function (post) {
+          return $('<li class="list-group-item">')
+            .text(post.title);
+        });
+        $('#posts').empty().append($posts);
       });
-  }
-
-  var siteManager = {
-    sites: [],
-    /**
-     * サイトを購読し、ポストを取得します
-     */
-    subscribe: function () {
-      // 入力されたサイトを取得
-      var siteId = $('#site_id').val();
-      $('#site_id').val('');
-
-      // 購読済みに追加
-      this.sites.push(siteId);
-
-      // 購読済みサイトとポストを更新
-      this.updateSites();
-      this.updatePosts();
-    },
-    /**
-     * 購読サイト一覧を更新します
-     */
-    updateSites: function () {
-      var $sites = this.sites
-        .map(function (site) {
-          return $('<li class="list-group-item">').text(site);
-        });
-      $('#sites').empty().append($sites);
-    },
-    /**
-     * 購読済みサイトのポスト一覧を更新します
-     */
-    updatePosts: function () {
-      // 購読済みサイトのポスト一覧を取得
-      var deferreds = this.sites
-            .map(function (site) {
-              return getPosts(site);
-            });
-
-      // すべてのサイトのポスト一覧の取得が終わってから処理
-      $.when.apply($, deferreds)
-        .then(function () {
-          // サイトごとのポストをフラットな配列に
-          var args = Array.prototype.slice.apply(arguments);
-          var posts = Array.prototype.concat.apply([], args);
-
-          // 日付順にソートする
-          posts.sort(function (a, b) {
-            var dateA = Date.parse(a.date);
-            var dateB = Date.parse(b.date);
-
-            return dateB - dateA;
-          });
-          console.log(posts);
-
-          // 表示を更新
-          var $posts = posts
-                .map(function (post) {
-                  return $('<li class="list-group-item">').text(post.title);
-                });
-          $('#posts').empty().append($posts);
-        });
-    }
-  };
-
-  $(document).on('submit', '#subscribe', siteManager.subscribe.bind(siteManager));
+  });
 })();
