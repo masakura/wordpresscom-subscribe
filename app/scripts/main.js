aa09d757fa902019812385cf5dcc03bdb245b124
Tomo Masakura <tomo.masakura@gmail.com>
2016-02-03 16:14:27 +0900

    WordPress.com にログインし、アクセストークンをもらえるところまで

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,36 @@
 (function () {
   'use strict';
 
+  // WordPress.com からアプリのキーを取得し、ここに書いてください
+  // 1. https://developer.wordpress.com/apps/new/ にアクセス
+  // 2. Name/Description は適当に入力
+  // 3. Website URL/Redirect URL/Javascript Origins にはアプリの URL を追加する
+  //   - Plunker のアプリの URL
+  // 4. Whats is ...? に答えを入れる
+  // 5. Type は `Web` を選択
+  // 6. `Create` ボタンをクリック
+  // 7. `Client ID` などが表示される
+  var clientId = 44553; // あなたの Client ID を書いてください
+
+  // アクセストークンなどを格納しておくための変数
+  var params = {};
+
+  // ダイアログのログインボタンがクリックされたら WordPress.com でログインさせる
+  // 1. `WordPress.com でログインする` をクリックする
+  // 2. WordPress.com のサイトが表示されるので、ログインし、`Approve` ボタンをクリックする
+  // 3. ページに戻ってくるので、URL にアクセストークンがあることを確認する
+  $(document).on('click', '#login', function () {
+    // 現在の URL をリダイレクト先に設定する
+    // http://localhost:9000 とか
+    var redirect = window.location.origin + window.location.pathname;
+    var location = 'https://public-api.wordpress.com/oauth2/authorize?client_id=' + clientId + '&redirect_uri=' + redirect + '&response_type=token';
+    window.location = location;
+  });
+
+  // アクセストークンがまだないときはログインボタンを持っているダイアログを表示します
+  if (!params['access_token']) { // eslint-disable-line dot-notation
+    $(document).ready(function () {
+      $('#login-modal').modal('show');
+    });
+  }
 })();
